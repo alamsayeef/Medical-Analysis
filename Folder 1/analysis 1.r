9d16532c169491b44b4ec1f2f93b4d73d272871a
alamsayeef <65837783+alamsayeef@users.noreply.github.com>
2020-07-23 15:50:45 +0530

    R code for anova
    See how the data was available and how the transformations were done prior to the analysis.

---
+++ Folder 1/analysis 1.r
@@ -0,0 +1,70 @@
+#provide appropiate path here
+d = read.csv("C:/Users/0295s/Desktop/sheet.csv") 
+
+#look into the data
+summary(d)
+
+#clean the data
+d$Sex = as.factor(d$Sex)
+d$CS_Headache = as.numeric(d$CS_Headache)
+d$CS_Vomitting = as.numeric(d$CS_Vomitting)
+d$Meningeal.sign = as.factor(d$Meningeal.sign)
+d$Neurologic.deficit = as.factor(d$Neurologic.deficit)
+d$CSF_Gstrain = as.factor(d$CSF_Gstrain)
+d$CSF_AFB = as.factor(d$CSF_AFB)
+d$CSF_Culture = as.factor(d$CSF_Culture)
+d$CSF_HSV.PCR = as.factor(d$CSF_HSV.PCR)
+d$CSF_ADA = as.factor(d$CSF_ADA)
+d$Outcome = as.factor(d$Outcome)
+d$Diagnosis = as.factor(d$Diagnosis)
+
+#look for further rectifications required
+summary(d)
+
+#attach the dataset, useful when you are working with just one dataset
+attach(d)
+
+############################
+#cytological parameters
+############################
+ar1= aov(CSF_TC~Diagnosis)
+summary(ar1)
+
+ar2 = aov(CSF_PMN~Diagnosis)
+summary(ar2)
+
+ar3 = aov(CSF_Lymphocyte~Diagnosis)
+summary(ar3)
+
+###########################
+#biochemical parameters
+############################
+ar4 = aov(CSF_Sugar~Diagnosis)
+summary(ar4)
+
+ar5 = aov(CSF_Protein~Diagnosis)
+summary(ar5)
+
+ar6 = aov(CSF_Lac_d1~Diagnosis)
+summary(ar6)
+
+ar7 = aov(CSF_Lac_d5~Diagnosis)
+summary(ar7)
+
+###########################
+#blood parameter parameters
+############################
+ar8 = aov(BP_TLC~Diagnosis)
+summary(ar8)
+
+ar9 = aov(BP_Neutrophil~Diagnosis)
+summary(ar9)
+
+ar10 = aov(BP_Lymphocyte~Diagnosis)
+summary(ar10)
+
+ar11 = aov(BP_RBS~Diagnosis)
+summary(ar11)
+
+ar12 = aov(BP_Lac_d1~Diagnosis)
+summary(ar12)
